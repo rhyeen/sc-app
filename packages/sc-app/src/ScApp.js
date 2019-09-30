cc1fd1f6f476de3a8afb78a45c47088735ac77c4
Ryan Saunders <>
2019-09-30 08:28:45 -0600

    works wonderfully!

--- packages/sc-app/src/ScApp.js
+++ packages/sc-app/src/ScApp.js
@@ -10,6 +10,11 @@ import { LitElement, html, css } from 'lit-element';
 // import { LOCALE_EN } from '../../internal_comps/sc_locale/src/entities/en.js';
 // import './Sc404.js';
 
+import { Log } from 'interface-handler/src/logger.js';
+
+import { Card } from '@shardedcards/sct-card/dist/browser/entities/card/sct-card.js';
+import { CardRarity } from '@shardedcards/sct-card/dist/browser/enums/sct-card-rarity.js';
+
 export class ScApp extends LitElement {
   static get styles() {
     return [
@@ -34,6 +39,8 @@ export class ScApp extends LitElement {
   }
 
   _activePageHtml() {
+    const card = new Card(CardRarity.Common);
+    Log.debug(card);
     switch (this._page) {
       // case ROUTES.PAGES.GAME:
       //   return html`<sc-game>HELLO WORLD</sc-game>`;
